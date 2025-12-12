d4881c4de4313f3ab851231cedbf977adcffe2be
karanmehta567 <karanmehta29.km@gmail.com>
2025-12-12 21:40:51 +0530

    Fixed Prisma Client

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,13 @@
 // lib/prisma.ts
 import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg"; // adapter for Postgres
+import { PrismaPg } from "@prisma/adapter-pg";
+import {Pool} from 'pg'
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!, // or pass config object the adapter expects
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
 });
+
+const adapter = new PrismaPg(pool);
 
 export const prisma = new PrismaClient({ adapter });
 export default prisma;
