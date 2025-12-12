c41628aa6d3413da5e603e4b7f8d6ab05925c04b
karanmehta567 <karanmehta29.km@gmail.com>
2025-12-12 21:19:50 +0530

    fixed typescript error

--- app/dashboard/courses/page.tsx
+++ app/dashboard/courses/page.tsx
@@ -13,7 +13,7 @@ export default async function Courses(){
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-3 gap-7">
             {
-                courses.map((course)=>(
+                courses.map((course:any)=>(
                     <AdminLayout data={course} key={course.id}/>
                 ))
             }
